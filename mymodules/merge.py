737ea2ddd7ef72c0a98364f9d484cbdc8e1ff035
rishabhjoshi <rj04796@live.com>
2016-09-22 22:54:07 +0530

    merge will only work on sets.. not terms

--- mymodules/merge.py
+++ mymodules/merge.py
@@ -8,78 +8,53 @@ except :
     import pickle
 
 # Globally define the index for use in the comparator
-indexfolder = '../index/'
-if not os.path.exists(indexfolder+'index.index'):
-    print ("No index exists. Please create the index first!")
-    sys.exit()
-index = pickle.load(open(indexfolder+'index.index', "rb"))
+#indexfolder = '../index/'
+#if not os.path.exists(indexfolder+'index.index'):
+#    print ("No index exists. Please create the index first!")
+#    sys.exit()
+#index = pickle.load(open(indexfolder+'index.index', "rb"))
 
-# Comparator used to sort the input terms list
-def comparator(term1, term2):
+# Comparator used to sort the input plists list
+def comparator(plist1, plist2):
     '''
-    Compare the terms' size of posting in index
+    Compare the plists' size of posting in index
     '''
-    if len(index[term1]) <= len(index[term2]):
+    if len(plist1) <= len(plist2):
         return -1
     else:
         return 1
 
-def AND(terms):
-    return operation(terms, 'and')
+def AND(plists):
+    return operation(plists, 'and')
 
-def OR(terms):
-    return operation(terms, 'or')
+def OR(plists):
+    return operation(plists, 'or')
 
-def DIFF(terms):
-    return operation(terms, 'diff')
+def DIFF(plists):
+    return operation(plists, 'diff')
 
-def SYMDIFF(terms):
-    return operation(terms, 'symdiff')
+def SYMDIFF(plists):
+    return operation(plists, 'symdiff')
 
-def NOT(terms):
-    return operation(terms, 'not')
+def NOT(plists):
+    return operation(plists, 'not')
 
-def setoperation(terms, flag):
-    if flag == 'not':
-        # Complement (NOT)a
-        doclist = glob.glob('./../corpus/*')
-        for i in range(len(doclist)):
-            doclist[i] = (doclist[i].strip('/')[-1]).strip('.')[0]
-        ans = set(doclist) - terms[0]
-        return ans
-    ans = terms[0]
-    for plist in terms[1:]:
-        if flag == 'and':
-            ans = ans & plist
-        elif flag == 'or':
-            ans = ans | plist
-        elif flag == 'diff':
-            ans = ans - plist
-        elif flag == 'symdiff':
-            ans = ans ^ plist
-    return ans         
-
-def operation(terms, flag):
+def operation(plists, flag):
     '''
-    This takes a list of terms (or sets), and merges the posting lists of these terms (or the sets, which are themselves postings)
+    This takes a list of plists (or sets), and merges the posting lists of these plists (or the sets, which are themselves postings)
     and returns a set of postings present in the merged set of the 
-    terms's list
+    plists's list
     '''
-    if type(terms[0]) == set:
-        # merging two postings and not terms
-        return setoperation(terms, flag)
-
     if flag == 'not':
         # Complement (NOT)a
         doclist = glob.glob('./../corpus/*')
         for i in range(len(doclist)):
             doclist[i] = (doclist[i].strip('/')[-1]).strip('.')[0]
-        ans = set(doclist) - index[terms[0]]
+        ans = set(doclist) - plists[0]
         return ans
-    terms = sorted(terms, cmp = comparator)
-    ans = index[terms[0]]   #postinglist of 1st term
-    for term in terms[1:]:
-        plist = index[term]
+    plists = sorted(plists, cmp = comparator)
+    ans = plists[0]   #postinglist of 1st plist
+    for plist in plists[1:]:
         if flag == 'and':
             ans = ans & plist
         elif flag == 'or':
@@ -91,6 +66,6 @@ def operation(terms, flag):
     return ans         
 
 if __name__ == '__main__':
-    print ("This module has functions to merge the posting lists of terms on conditinos such as AND, OR, DIFF, SYMDIFF")
-    print ("The functions of this module can be used by passing a list of terms to \n   -- AND\n   -- OR\n   -- DIFF\n   -- SYMDIFF\n   -- NOT")
+    print ("This module has functions to merge the posting lists on conditinos such as AND, OR, DIFF, SYMDIFF")
+    print ("The functions of this module can be used by passing a list of posting to \n   -- AND\n   -- OR\n   -- DIFF\n   -- SYMDIFF\n   -- NOT")
     sys.exit()
